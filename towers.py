4a87b26e003658770a29ad794adbc3babbaf2ee7
Derek <derekforgione@gmail.com>
2021-07-14 21:28:39 -0700

    Added a comment to TowersGame class.
    The comment describes components that will be necessary for the
    game to run.

--- towers.py
+++ towers.py
@@ -1,3 +1,15 @@
 """
 This class contains classes relevant to the Towers of Hanoi game.
 """
+
+class TowersGame:
+	"""
+	This class is for running the Towers of Hanoi game.
+	"""		
+	
+	# What we will need:
+	# Buttons
+	# Pegs (Towers)
+	# Disks
+
+	pass
